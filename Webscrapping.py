fcb537da3e5a25543c1b31ac7513c27f8dbc2956
Naum Mazor <maznam1@mail.ru>
2021-12-08 10:39:10 +0800

    Added Webscrapping.py

---
+++ Webscrapping.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+MY_HUBS = ['дизайн', 'фото', 'web', 'python']
+
+response = requests.get("https://habr.com/ru/all/", timeout=15, headers={"User-Agent":"Mozilla/5.0 "})
+response.raise_for_status()
+text = response.text
+
+soup = BeautifulSoup(text, features="html.parser")
+articles = soup.find_all('article')
+
+title_1 = ""
+for article in articles:
+    for hub in MY_HUBS:
+        pattern_hub = re.compile("({})".format(hub))
+        result = pattern_hub.findall(article.text)
+        if hub in result:
+            date = article.find('time').attrs.get('title')
+            title = article.find('h2')
+            link = title.find('a').attrs.get('href')
+            url = "https://habr.com" + link
+            if title.text != title_1:
+                print(f'{date[0:10]} - {title.text} - {url}')
+                title_1 = title.text
